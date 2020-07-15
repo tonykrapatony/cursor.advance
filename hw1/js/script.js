b5a49fb5cca636cea9acd8221e9aec73ae4dcd55
Ihor <tonykrapatony@gmail.com>
2020-07-15 12:10:41 +0300

    hw 1 done

--- hw1/js/script.js
+++ hw1/js/script.js
@@ -6,7 +6,7 @@ window.onload = function() {
         minNum = Math.min(num1, num2, num3),
         sumNum = num1+num2+num3,
         sumRound = Math.round(num1)+Math.round(num2)+Math.round(num3),
-        sumRound100 =  Math.ceil(sumNum / 100) * 100,
+        sumRound100 =  Math.round(sumNum / 100) * 100,
         sumIsEven ,
         change = 500 - sumNum,
         average = (sumNum/3).toFixed(2),
@@ -21,8 +21,11 @@ window.onload = function() {
     }
     
     document.writeln("<h3>Basic</h3>"); 
+    document.writeln("<p>Перше число: "+num1+"</p>"); 
+    document.writeln("<p>Друге число: "+num2+"</p>"); 
+    document.writeln("<p>Третє число: "+num3+"</p>"); 
     document.writeln("<p>Максимальне число: "+maxNum+"</p>"); 
-    document.writeln("<p>Максимальне число: "+minNum+"</p>"); 
+    document.writeln("<p>Мінімальне число: "+minNum+"</p>"); 
     document.writeln("<p>Сума чисел: "+sumNum+"</p>"); 
     document.writeln("<p>Сума чисел округлена до меншого: "+sumRound+"</p>"); 
     document.writeln("<p>Сума чисел округлена до сотень: "+sumRound100+"</p>"); 
@@ -32,13 +35,20 @@ window.onload = function() {
     document.writeln("<p>Випадкова знижка: "+randomDiscount+"</p>"); 
     document.writeln("<p>Вартість зі знижкою: "+dicountPrice+"</p>"); 
     document.writeln("<p>Прибуток: "+profit+"</p>"); 
-    document.writeln("<p>Прибуток: "+profit+"</p>"); 
     document.writeln("<h3>Advanced</h3>"); 
     document.writeln("<p class="+"advanced"+"></p>");
     
-    const advanced = '<p>Максимальна ціна: ${maxNum}</p>
-                      <p>Мінімальна ціна: ${minNum}</p>
-                      <p>Вартість товарів: ${sumNum}</p>';
+    var advanced = `<p>Максимальне число: ${maxNum}</p>
+                    <p>Мінімальне число: ${minNum}</p>
+                    <p>Сума чисел: "${sumNum}</p>
+                    <p>Сума чисел округлена до меншого: ${sumRound}</p>
+                    <p>Сума чисел округлена до сотень: ${sumRound100}</p>
+                    <p>Округлена сума парна?: ${sumIsEven}</p>
+                    <p>Решта при оплаті 500 грн: ${change}</p>
+                    <p>Середнє значення цін, округлене до другого знаку після коми: ${average}</p>
+                    <p>Випадкова знижка: ${randomDiscount}</p>
+                    <p>Вартість зі знижкою: ${dicountPrice}</p>
+                    <p>Прибуток: ${profit}</p>`;
     
-    document.querySelector('.advanced').innerHTML = advanced,
+    document.querySelector('.advanced').innerHTML = advanced;
 };
