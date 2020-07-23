dd80d34f7310a04f4d4e7f19bc37b9a9b483d276
Ihor <tonykrapatony@gmail.com>
2020-07-23 19:18:50 +0300

    continuation of hw3

--- hw3/js/script.js
+++ hw3/js/script.js
@@ -1,4 +1,5 @@
-document.getElementById("getMax").addEventListener('click', function(){
+// fn1
+document.querySelector('.func1 button').addEventListener('click', function(){
     const number = document.querySelector('.number').value; 
     function getMaxDigit(number){
         let max = 0;
@@ -12,8 +13,8 @@ document.getElementById("getMax").addEventListener('click', function(){
 
     getMaxDigit(number);
 })
-
-document.getElementById("calc").addEventListener('click', function(){
+//fn2
+document.querySelector('.func2 button').addEventListener('click', function(){
     const basis = document.querySelector('.basis').value; 
     const degree = document.querySelector('.degree').value; 
     function toDegree(basis, degree){
@@ -35,8 +36,8 @@ document.getElementById("calc").addEventListener('click', function(){
 
     toDegree(basis, degree);
 })
-
-document.getElementById("format").addEventListener('click', function(){
+//fn3
+document.querySelector('.func3 button').addEventListener('click', function(){
     const name = document.querySelector('.name').value; 
     function formatName(name){
         let format = '';
@@ -48,6 +49,76 @@ document.getElementById("format").addEventListener('click', function(){
 
     formatName(name);
 })
+//fn4
+document.querySelector('.func4 button').addEventListener('click', function(){
+    const number = document.querySelector('.func4 input').value;
+    
+    function formatName(number){
+        let result = number - ((number*19.5)/100);
+        return document.querySelector('.func4 span').innerHTML = ' = ' + result;
+    }
 
+    formatName(number);
+})
+//fn5
+document.querySelector('.func5 button').addEventListener('click', function(){
+    const min = document.querySelector('.func5 .min').value; 
+    const max = document.querySelector('.func5 .max').value; 
+    
+    
+    function getRandomNumber(min, max) {
+        let result = Math.ceil((Math.random() * (max - min) + min));
+        return document.querySelector('.func5 span').innerHTML = ' = ' + result;
+    }
+    
+    getRandomNumber(min, max);
+})
+//fn6
+document.querySelector('.func6 button').addEventListener('click', function(){
+    const startWord = document.querySelector('.word').value; 
+    const startLetter = document.querySelector('.letter').value; 
+    
+    function countLetter(startWord, startLetter) {
+        let result = 0;
+        let word = '';
+        let letter = startLetter.toLowerCase();
+        
+        for (let i=0; i<startWord.length; i++){
+            word += startWord[i].toLowerCase();
+        }
+        for (let i=0; i<word.length; i++){
+            if (word[i] === letter){
+                result++;
+            }
+        }
+        
+        return document.querySelector('.func6 span').innerHTML = ' = ' + result;
+    }
+    
+    countLetter(startWord, startLetter);
+})
+//fn7
+document.querySelector('.func8 button').addEventListener('click', function(){
+    const n = document.querySelector('.func8 input').value; 
+    
+    function getRandomPassword(n) {
+        let password = '';
+        
+        if (n <= 0){
+            for (let i=0; i<8; i++){
+                randomNum = Math.ceil((Math.random() * (9 - 0) + 0));
+                password += randomNum;
+            }
+        } else {        
+            for (let i=0; i<n; i++){
+                randomNum = Math.ceil((Math.random() * (9 - 0) + 0));
+                password += randomNum;
+            }
+        }
+        
+        return document.querySelector('.func8 span').innerHTML = ' = ' + password;
+    }
+    
+    getRandomPassword(n);
 
-
+})
