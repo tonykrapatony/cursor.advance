f568e1c5550a13112b7231549051b0616cb8e7ef
Ihor <tonykrapatony@gmail.com>
2020-08-11 18:08:54 +0300

    hw8 advance start

--- hw8/js/script.js
+++ hw8/js/script.js
@@ -1,6 +1,6 @@
 class Student {
     
-    constructor( course,university, fullName){
+    constructor( course, university, fullName){
         this.university = university;
         this.course = course;
         this.fullName= fullName;
@@ -52,3 +52,29 @@ ostap.dismiss();
 ostap.marks;
 ostap.recover();
 ostap.marks;
+
+class newStudent extends Student {
+    constructor( course,university, fullName){
+        super(course,university, fullName);
+        (setInterval(() =>this.scholarship, 3000));
+    }
+    get scholarship(){
+        if(this.getAverageMark() < 4) {
+            console.log("У вас нема стипендії");
+        } else {
+            console.log('Ви отримали 1400 грн. стипендії');
+        }
+    }
+}
+
+const ihor = new newStudent("1 курсу","Вищої Школи Психотерапії м.Одеса","Ігор Виноградний");
+
+ihor.getInfo();
+ihor.marks;
+ihor.marks = 3;
+ihor.marks;
+ihor.getAverageMark();
+ihor.dismiss()
+ihor.marks;
+ihor.recover();
+ihor.marks;
