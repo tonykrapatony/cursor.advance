be42a5eb6ee77b7fd08506e7776b77cc5130dd54
Ihor <tonykrapatony@gmail.com>
2020-08-20 18:03:13 +0300

    hw 11

---
+++ hw11/js/script.js
@@ -0,0 +1,23 @@
+const waitTime = 50;
+
+function getRandomChines(length) {
+    return new Promise((resolve) => {
+        let answer = "";
+        let a = 0;
+        let numberForChar = parseInt(Date.now().toString().slice(-5));
+//        for (let a=0; a<length; a++){
+//            numberForChar += length;
+//            answer += String.fromCharCode(numberForChar);
+//        }
+        while (a < length) {
+            numberForChar += 1;
+            answer += String.fromCharCode(numberForChar);
+            a++;
+        }
+        setTimeout(() =>
+            resolve(console.log(answer))
+        , length * waitTime);
+    });
+};
+
+getRandomChines(4);
